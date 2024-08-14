964c5d65add2a484dbf881dec88b8f2eadc4b647
singsangssong <plmko0914@gmail.com>
2024-08-14 20:32:12 +0900

    레디스 통신 개발 중. 꼭 작업 끝나면 커밋하기

--- database/src/main/java/org/example/database/chatroom/ChatRoom.java
+++ database/src/main/java/org/example/database/chatroom/ChatRoom.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.example.database.chatroom.dto.ChatRoomDto;
 import org.example.database.common.BaseEntity;
 import org.example.database.jointable.ChatRoom_Member;
 import org.example.database.member.Member;
@@ -32,4 +33,7 @@ public class ChatRoom extends BaseEntity {
         this.name = name;
     }
 
+    public ChatRoomDto toDto(ChatRoom chatRoom) {
+        ChatRoomDto chatRoomDto = new ChatRoomDto(chatRoom);
+    }
 }
--- database/src/main/java/org/example/database/chatroom/dto/ChatRoomDto.java
+++ database/src/main/java/org/example/database/chatroom/dto/ChatRoomDto.java
@@ -1,7 +1,19 @@
 package org.example.database.chatroom.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+import org.example.database.chatroom.ChatRoom;
+
+@Getter
+
 public class ChatRoomDto {
     private String roomId;
     private String roomName;
-    private Long ownerId;
+//    private Long ownerId;
+
+    public ChatRoomDto (ChatRoom chatRoom) {
+        this.roomName = chatRoom.getName();
+        this.roomId = chatRoom.getId().toString();
+//        this.ownerId =
+    }
 }
